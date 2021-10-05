cad2712ff2182d5c102d366ac85aded6a1a65f96
Sergey Maslov <maslovss@gmail.com>
2021-10-05 22:25:01 +0300

    fix1

--- compute.tf
+++ compute.tf
@@ -17,8 +17,41 @@ resource "aws_instance" "master" {
 
 
   provisioner "local-exec" {
-    command = "echo ${self.public_ip} > master.txt"
+
+    command = <<-EOT
+      echo "[master]" > aws_hosts
+      echo ${self.public_ip} >> aws_hosts
+    EOT
   }
    
+}
 
+
+resource "aws_instance" "workers" {
+
+  for_each  =  var.kube_workers    
+
+  depends_on = [
+    aws_security_group.sg_k8s , aws_instance.master
+  ]
+
+  ami = "${each.value.ami}"
+  instance_type = "${each.value.instance_type}"
+
+  key_name = var.key_name
+
+  vpc_security_group_ids = [aws_security_group.sg_k8s.id]
+  subnet_id = aws_subnet.public_subnet.id
+
+  tags = var.tags  
+
+
+  provisioner "local-exec" {
+
+    command = <<-EOT
+      echo "[workers]" >> aws_hosts
+      echo ${self.public_ip} >> aws_hosts
+    EOT
+  }
+   
 }
