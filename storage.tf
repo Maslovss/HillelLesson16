f7303356b944eba396936f5e65c7198ce9fbf5d9
Sergey Maslov <maslovss@gmail.com>
2021-10-06 14:29:49 +0300

    volumes adding for workers

--- storage.tf
+++ storage.tf
@@ -1,11 +1,17 @@
 
-resource "aws_ebs_volume" "example" {
+resource "aws_ebs_volume" "volumes" {
+
+  for_each  =  var.kube_workers 
+
   availability_zone = "${aws_subnet.public_subnet.availability_zone}"
-  size              = 20
+  size              = each.value.ebs_drive_size
 }
 
 resource "aws_volume_attachment" "ebs_att" {
+
+  for_each  =  var.kube_workers 
+
   device_name = "/dev/xvdb"
-  volume_id   = aws_ebs_volume.example.id
-  instance_id = aws_instance.master.id
+  volume_id   = lookup(aws_ebs_volume.volumes , each.key).id
+  instance_id = lookup(aws_instance.workers, each.key).id
 }
