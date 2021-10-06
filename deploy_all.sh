faaedc61adfd33b13293cba4c530abb3801d847e
Sergey Maslov <maslovss@gmail.com>
2021-10-06 15:55:58 +0300

    fix

--- deploy_all.sh
+++ deploy_all.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+terraform apply -auto-approve
+
 ansible-playbook --private-key k8s_ssh_key.pem -u ubuntu -i aws_hosts deploy_packages.yaml
 ansible-playbook --private-key k8s_ssh_key.pem -u ubuntu -i aws_hosts deploy_cluster.yaml
 ansible-playbook --private-key k8s_ssh_key.pem -u ubuntu -i aws_hosts deploy_nodes.yaml
